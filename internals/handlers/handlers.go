2450ed35610a3e1cc057baf0e308bec9eacfc723
Nayyer Shahzad <nayyer931@yahoo.com>
2022-12-24 13:59:22 +0400

    server side testing

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/nayyershahzad/bookings/internals/config"
+	"github.com/nayyershahzad/bookings/internals/forms"
 	"github.com/nayyershahzad/bookings/internals/models"
 	"github.com/nayyershahzad/bookings/internals/render"
 )
@@ -56,7 +57,14 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "make-reservation.page.tmpl", r, &models.TemplateData{})
+	render.RenderTemplate(w, "make-reservation.page.tmpl", r, &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page
--- pkg/models/templatedata.go
+++ pkg/models/templatedata.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/nayyershahzad/bookings/internals/forms"
+
 //TemplateData holds data to be sent to templates from handlers
 type TemplateData struct {
 	StringMap map[string]string
@@ -10,4 +12,5 @@ type TemplateData struct {
 	Flash     string
 	Warning   string
 	Error     string
+	Form      *forms.Form
 }
