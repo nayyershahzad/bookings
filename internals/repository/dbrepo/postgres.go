07047b587ecc57edc22df6ad678c48fe5a2132d6
Nayyer Shahzad <nayyer931@yahoo.com>
2023-01-17 22:35:12 +0400

    106

--- internals/repository/dbrepo/postgres.go
+++ internals/repository/dbrepo/postgres.go
@@ -12,15 +12,15 @@ func (m *postgresDbRepo) AllUsers() bool {
 }
 
 // InsertReservation inserts the reservation into the DB
-func (m *postgresDbRepo) InsertReservation(res models.Reservation) error {
+func (m *postgresDbRepo) InsertReservation(res models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-
 	defer cancel()
+	var newID int
 	stmt := `insert into reservations (first_name, last_name, email,phone,start_date,
-			end_date, room_id,start_date, end_date)
-			values ($1,$2,$3,$4,$5,$6,$7,$8,$9)`
-	_, err := m.DB.ExecContext(ctx, stmt,
-		res.EndDate,
+			end_date, room_id,created_at, updated_at)
+			values ($1,$2,$3,$4,$5,$6,$7,$8,$9) returning id`
+	err := m.DB.QueryRowContext(ctx, stmt,
+		res.FirstName,
 		res.LastName,
 		res.Email,
 		res.Phone,
@@ -28,7 +28,29 @@ func (m *postgresDbRepo) InsertReservation(res models.Reservation) error {
 		res.EndDate,
 		res.RoomId,
 		time.Now(),
-		time.Now())
+		time.Now(),
+	).Scan(&newID)
+	if err != nil {
+		return 0, err
+	}
+	return newID, nil
+}
+
+func (m *postgresDbRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	stmt := `insert into room_restrictions (start_date, end_date, room_id, reservation_id, created_at, 
+			updated_at, restriction_id)
+			values ($1,$2,$3,$4,$5,$6,$7)`
+	_, err := m.DB.ExecContext(ctx, stmt,
+		r.StartDate,
+		r.EndDate,
+		r.RoomId,
+		r.ReservationID,
+		time.Now(),
+		time.Now(),
+		r.RestrictionID,
+	)
 	if err != nil {
 		return err
 	}
